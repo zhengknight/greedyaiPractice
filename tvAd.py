9bbac497ade23c2f9a5a5eeb2c20a8895845ba29
zhengknight <zhengknight@qq.com>
2019-06-27 17:03:20 +0800

    代码实现

--- tvAd.py
+++ tvAd.py
@@ -5,4 +5,38 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
+data = pd.read_csv("Advertising.csv")
+#输出文件头
+#print(data.head())
+#输出文件列名称
+#print(data.columns)
+
+#显示数据散点图
+# plt.figure(figsize=(16,8))
+# plt.scatter(data['TV'],data['sales'],c='black')
+# plt.xlabel("Money spent on TV ads")
+# plt.ylabel("Sales")
+# plt.show()
+#训练线性回归模型
+x = data['TV'].values.reshape(-1,1)
+y = data['sales'].values.reshape(-1,1)
+#print(x)
+reg = LinearRegression()
+reg.fit(x,y)
+
+print('a = {:.5}'.format(reg.coef_[0][0]))
+print('b = {:.5}'.format(reg.intercept_[0]))
+print("线性模型为：Y={:.5}X+{:.5}".format(reg.coef_[0][0],reg.intercept_[0]))
+
+#可视化训练好的线性回归模型
+predictions = reg.predict(x)
+plt.figure(figsize=(16,8))
+plt.scatter(data['TV'],data['sales'],c='black')
+plt.plot(data['TV'],predictions,c='blue',linewidth=2)
+plt.xlabel("Money spent on TV ads")
+plt.ylabel("Sales")
+plt.show()
+
+#做预测，假设下一年度公司想投入
